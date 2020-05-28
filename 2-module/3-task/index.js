33e0a9953179cf8b033bbe2f5539cfa71c751bad
Panov Alexey <alexeykapanov@gmail.com>
2020-05-28 20:30:12 +0300

    Solve tasks for the second module.

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
-  // ваш код
+  firstValue: Number = 0,
+  secondValue: Number = 0,
+
+  read(firstValue, secondValue) {
+    this.firstValue = firstValue;
+    this.secondValue = secondValue;
+  },
+  sum() {
+    return this.firstValue + this.secondValue;
+  },
+  mul() {
+    return this.firstValue * this.secondValue;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
