3f590f47f2d309fb65e4e72cd3446c1b50662f3b
PanovAlexey <alexeykapanov@gmail.com>
2020-06-25 12:41:00 +0300

    Solve the third task for the third module.

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,6 +1,18 @@
 /**
- * @param {string} str
+ * @param {string} inputString
  * @returns {string}
  */
-function camelize(str) {
+function camelize(inputString) {
+  let arrayWithStringByDash = inputString.split('-');
+  let camelCaseString = '';
+
+  for (let i = 0; i < arrayWithStringByDash.length; i++) {
+    if (i > 0) {
+      camelCaseString += arrayWithStringByDash[i][0].toUpperCase() + arrayWithStringByDash[i].slice(1, arrayWithStringByDash[i].length);
+    } else {
+      camelCaseString += arrayWithStringByDash[i];
+    }
+  }
+
+  return camelCaseString;
 }
