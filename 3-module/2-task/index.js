5b8b845b7fafe98b06d100edd66e417111d3ff8c
PanovAlexey <alexeykapanov@gmail.com>
2020-06-27 14:53:34 +0300

    Solve the second task for the third module.

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,7 +1,24 @@
 /**
  * Найти min/max в произвольной строке
- * @param   {string} str -  входные данные
+ * @param   {string} inputString -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+function getMinMax(inputString) {
+  const arraySplitByComma = inputString.replace(/ /g, ',').split(',');
+  let arrayNumbers = arraySplitByComma.filter(
+    value => isFinite(value) && value !== ''
+  ).map(
+    value => +value
+  );
+
+  let sortedArrayNumbers = arrayNumbers.sort(
+    (valueOne, valueSecond) => valueOne - valueSecond
+  );
+
+  let result = {
+    min: sortedArrayNumbers[0],
+    max: sortedArrayNumbers[sortedArrayNumbers.length - 1]
+  }
+
+  return result;
 }
