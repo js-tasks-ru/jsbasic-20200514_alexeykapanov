b4b3c524cdd05d3f9f94d9a3cee375d3b9621449
PanovAlexey <alexeykapanov@gmail.com>
2020-06-29 10:16:06 +0300

    Solve the second task for the fourth module.

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,13 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  if (table.rows.length !== table.rows[0].cells.length) {
+    throw 'The matrix is not square. Only a square matrix can have a diagonal.';
+  }
+
+  let redColorName = 'red';
+
+  Array.from(table.rows).forEach(function (row, rowNumber, rows) {
+    row.cells[rowNumber].style.backgroundColor = redColorName;
+  });
 }
